537b60f2b721508de9efbfa0011e2dc5a9e57e7f
slethron <slethron@gmail.com>
2011-05-30 16:02:06 +0000

    added an add function

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
+int add(int num1, int num2)
+{
+	return num1 + num2;
+}
+
+
 
 int main()
 {
-	int x,y,selection;
+	int x,y,selection,result;
 
 	printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo operation\n");
 
 	scanf("%d",&selection);
+   
+   printf("Please enter two integers:");
+   
+   scanf("%d %d",&x,&y);
 
 	if(selection == 1){
-	//add function call
+         result=add(x,y);
+			printf(("%d",result);
 	}
 	else if(selection == 2){
 	//subtract func call
