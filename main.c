59fdceb2ded55cfb0289a50267343c7009068bd1
oguzdemir3 <oguzdemir3@gmail.com>
2011-05-30 09:38:29 +0000

    created the main file and implemented control part

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+
+int main()
+{
+int x,y,selection;
+printf("Press 1 for addition operation, 2 for subtraction, 3 for multiplication and 4 for division:");
+scanf("%d",&selection);
+if(selection == 1){
+//add function call
+}
+else if(selection == 2){
+//subtract func call
+}
+else if(selection == 3){
+//mult func call
+}
+else if(selection == 4){
+//div func call
+}
+
+return 0;
+}
+
