dc12906c1422446e34f17427f63dd7f92335d7c5
setiva <setiva@gmail.com>
2011-05-30 13:38:56 +0000

    modulo operation option is added.

--- main.c
+++ main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 
 
-
-
 int main()
 {
-int x,y,selection;
-printf("Press 1 for addition operation, 2 for subtraction, 3 for multiplication and 4 for division:");
-scanf("%d",&selection);
-if(selection == 1){
-//add function call
-}
-else if(selection == 2){
-//subtract func call
-}
-else if(selection == 3){
-//mult func call
-}
-else if(selection == 4){
-//div func call
-}
+	int x,y,selection;
 
-return 0;
+	printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo operation\n");
+
+	scanf("%d",&selection);
+
+	if(selection == 1){
+	//add function call
+	}
+	else if(selection == 2){
+	//subtract func call
+	}
+	else if(selection == 3){
+	//mult func call
+	}
+	else if(selection == 4){
+	//div func call
+	}
+	else if(selection == 5){
+	//modulo func call
+	}
+
+	return 0;
 }
 
