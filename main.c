7df94eaf849fb7a5379561095a4dff7457f53054
Ozan <Ozan@Ozan-PC.superdorm.dorm.boun.edu.tr>
2011-06-01 22:24:12 +0300

    Added power function.

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int add(int num1, int num2)
 {
         return num1 + num2;
 }
 int subtract(int num1, int num2)
-{       
+{
         return num1 - num2;
 }
 int modulo(int num1, int num2)
@@ -18,6 +19,11 @@ int multiplication(int num1,int num2)
         return num1*num2;
 }
 
+int power(int num1, int num2)
+{
+        return pow(num1,num2);
+}
+
 int division(int num1,int num2)
 {
         return num1/num2;
@@ -27,12 +33,12 @@ int main()
 {
         int x,y,selection,result;
 
-        printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo operation\n");
+        printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo\n\t6 for power operation\n");
 
         scanf("%d",&selection);
-   
+
    printf("Please enter two integers:");
-   
+
    scanf("%d %d",&x,&y);
 
         if(selection == 1){
@@ -46,7 +52,7 @@ int main()
                 printf("%d\n",result);
         }
         else if(selection == 3){
-         
+
                result = multiplication(x,y);
                printf("%d\n",result);
         }
@@ -58,12 +64,16 @@ int main()
                }
         }
         else if(selection == 5){
-        
+
                         if(y != 0)
                         {
                           result = modulo(x,y);
                           printf("%d\n",result);
                         }
+        }
+        else if(selection == 6){
+            result = power(x,y);
+            printf("%d\n",result);
         }
 
         return 0;
