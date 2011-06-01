09feb258e3d466b784c21c5732ab260706fb087d
setiva <setiva@gmail.com>
2011-06-01 10:05:13 +0000

    modulo operation is implemented.

--- main.c
+++ main.c
@@ -2,43 +2,55 @@
 
 int add(int num1, int num2)
 {
-	return num1 + num2;
+        return num1 + num2;
 }
 int subtract(int num1, int num2)
-{	
-	return num1 - num2;
+{       
+        return num1 - num2;
 }
+int modulo(int num1, int num2)
+{
+	return num1 % num2;
+}
+
 
 int main()
 {
-	int x,y,selection,result;
+        int x,y,selection,result;
 
-	printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo operation\n");
+        printf("Press   1 for addition operation\n\t2 for subtraction\n\t3 for multiplication\n\t4 for division\n\t5 for modulo operation\n");
 
-	scanf("%d",&selection);
+        scanf("%d",&selection);
    
    printf("Please enter two integers:");
    
    scanf("%d %d",&x,&y);
 
-	if(selection == 1){
-         result=add(x,y);
-			printf("%d",result);
-	}
-	else if(selection == 2){
-		result=subtract(x,y);
-		printf("%d",result);
-	}
-	else if(selection == 3){
-	//mult func call
-	}
-	else if(selection == 4){
-	//div func call
-	}
-	else if(selection == 5){
-	//modulo func call
-	}
+        if(selection == 1){
 
-	return 0;
+                result=add(x,y);
+                printf("%d\n",result);
+        }
+        else if(selection == 2){
+
+                result=subtract(x,y);
+                printf("%d\n",result);
+        }
+        else if(selection == 3){
+        //mult func call
+        }
+        else if(selection == 4){
+        //div func call
+        }
+        else if(selection == 5){
+        
+			      if(y != 0)
+			      {
+			          result = modulo(x,y);
+			          printf("%d\n",result);
+			      }
+        }
+
+        return 0;
 }
 
