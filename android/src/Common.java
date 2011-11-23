3b8fd77fe2e26467a5b4967b5f41bf4999d5f62a
oguzdemir3 <oguzdemir3@localhost>
2011-11-23 16:00:10 +0200

    android klasoru kuruldu, android ile alakali butun dosyalar bu klasorun altinda olsun lutfen

---
+++ android/src/Common.java
@@ -0,0 +1,7 @@
+package mass.observation;
+
+public class Common {
+	public static String dbUrl = "jdbc:mysql://localhost:3306/mob";
+	public static String username = "root";
+	public static String password = "oguz";
+}
