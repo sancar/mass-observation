8d0e87e66b212f1d3ef4308616cc9887ba96f558
oguzdemir3 <oguzdemir3@localhost>
2011-11-23 16:00:10 +0200

    android klasoru kuruldu, android ile alakali butun dosyalar bu klasorun altinda olsun lutfen

--- Source Codes/MyServlet.java
+++ Source Codes/MyServlet.java
@@ -34,7 +34,40 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		String email = "", password1 = "";
+		
+		PrintWriter out = response.getWriter();
+		
+		if(request.getParameter("email") != null && request.getParameter("password1") != null){
+			email = request.getParameter("email");
+			password1 = request.getParameter("password1");
+			
+			if(!email.isEmpty() && !password1.isEmpty()){
+				try {
+					Connection connection = DriverManager.getConnection(dbUrl , username, password);
+					Statement statement = connection.createStatement();
+					
+					ResultSet result = 	statement.executeQuery("SELECT name FROM users WHERE email = '"+ email +"' AND activation_key = '1' AND password  =  '"+ password1+ "'");
+		
+					 if(result.next()){
+						 HttpSession session = request.getSession();
+						 session.setAttribute("email", email);
+						 session.setAttribute("name", (String)result.getObject(0));
+						 response.sendRedirect("/welcome.jsp");
+					 }else{
+						out.print("There is no account with matching email and password");
+					 }
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				out.print("please enter email and password to login");
+			}
+		}else{
+			out.print("invalid input to login");
+		}
 	}
 
 }
