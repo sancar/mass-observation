618f3cabc081bc8aae03c95f10dec04815b11449
Betul <Betul@Betul-PC>
2011-12-13 17:41:35 +0200

    Adding comment to observations are done.

--- mob-web/src/java/newpackage/AddComment.java
+++ mob-web/src/java/newpackage/AddComment.java
@@ -1,4 +1,7 @@
-
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package newpackage;
 
 import java.io.IOException;
@@ -12,50 +15,73 @@ import javax.servlet.http.HttpSession;
 
 import java.io.PrintWriter;
 import java.sql.*;
-import java.util.Date;
-import java.text.*;
 
-@WebServlet("/AddText")
-public class AddText extends HttpServlet {
-    
+/**
+ *
+ * @author Betul
+ */
+@WebServlet(name = "AddComment", urlPatterns = {"/AddComment"})
+public class AddComment extends HttpServlet {
 
-    private static final long serialVersionUID = 1L;
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     private static final long serialVersionUID = 1L;
     String dbUrl = "jdbc:mysql://titan.cmpe.boun.edu.tr:3306/database3";
     String username = "project3";
     String password = "i52jm";
     
-
+    
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+      
     }
 
-
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+       
         HttpSession session = request.getSession();
         String name = (String) session.getAttribute("name");
         String email = (String) session.getAttribute("email");
         
-        Date date=new Date();
-        SimpleDateFormat sdf=new SimpleDateFormat("MM/dd/yy");
         
-        String text_id="";
-        String text="";
+        
+        
+        String comment=request.getParameter("Comment");
         String event_id=request.getParameter("event_id");
-        String supplied_by=email;
-        String comment_id="";
-        String score="0";
+        String owner=email;
+        String observation_id="";
+        String observation_type="";
         String name_visible;
         if("anonymous".equals(request.getParameter("anonymous"))) name_visible="0";
         else name_visible="1";
         
-       // String date_added=sdf.format(date);
         
-        text = request.getParameter("textOE");
+        if(!(request.getParameter("text_id")==null))
+        {
+            observation_id=request.getParameter("text_id");
+            observation_type = "text";
+        }
+        else if(!(request.getParameter("poll_id")==null))
+        {
+            observation_id=request.getParameter("poll_id");
+            observation_type = "poll";
+        }
+        
 
         PrintWriter out = response.getWriter();
         
@@ -70,9 +96,9 @@ public class AddText extends HttpServlet {
             Statement statement;
             statement=connection.createStatement();
             
-            String sql="INSERT INTO observations_text( text, event_id, supplied_by, comment_id, score"
-                    + ", name_visible)"
-                    + " VALUES('"+ text + "',"+event_id+ ",'"+email+"',0,"+score+","+name_visible+" )";
+            String sql="INSERT INTO comment_owner( owner, name_visible, comment, observation_type, observation_id)"
+                   
+                    + " VALUES('"+ owner + "',"+name_visible+ ",'"+comment+"','"+observation_type+"',"+observation_id+" )";
             statement.executeUpdate(sql);
 
 
@@ -90,10 +116,14 @@ public class AddText extends HttpServlet {
         session.setAttribute("email", email);
         session.setAttribute("name", name);
         response.sendRedirect("./observeOE.jsp?id="+event_id);
-        
-        }
- 
     }
 
-
-
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
