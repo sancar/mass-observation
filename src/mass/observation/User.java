d91e70c0961e6eab6c6a330b9e2a4cd5529adfa2
ozanirsoy <ozanirsoy@localhost>
2011-12-29 02:41:38 +0200

    Comments are added to User.java

--- src/mass/observation/User.java
+++ src/mass/observation/User.java
@@ -1,17 +1,46 @@
 package mass.observation;
 
+/** 
+ * A class to keep basic user info.
+ * 
+ *  @author Oguz Demir
+ *  @version 1.0
+ *  
+ */
+
 public class User {
 	private static String username;
 	private static String password;
+	
+	
+	/**
+	 * Sets the username of this user.
+	 * @param username Username of this user.
+	 */
 	public static void setUsername(String username) {
 		User.username = username;
 	}
+	
+	/**
+	 * Gets the username of this user.
+	 * @return Username of this user.
+	 */
 	public static String getUsername() {
 		return username;
 	}
+	
+	/**
+	 * Sets the password of this user.
+	 * @param password Password of this user.
+	 */
 	public static void setPassword(String password) {
 		User.password = password;
 	}
+	
+	/**
+	 * Gets the password of this user.
+	 * @return Password of this user.
+	 */
 	public static String getPassword() {
 		return password;
 	}
